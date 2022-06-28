614d2930f1e2c5080ebc3de67cc7fdb6e72655f4
Conner <65339198+connercsbn@users.noreply.github.com>
2022-06-27 20:46:15 -0700

    make persistent cursor paint canvas thingy

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,36 +1,61 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import Cookies from 'js-cookie';
 	let message = '';
-	let ws;
-	let canvas;
-	let ctx;
+	let ws: WebSocket;
+	let canvas: HTMLCanvasElement;
+	let ctx: CanvasRenderingContext2D | null;
 	type MousePos = {
 		x: number;
 		y: number;
 	};
-	const fillPixel = (x, y) => {
-		ctx.fillStyle = 'rgba(' + 0 + ',' + 0 + ',' + 0 + ',' + 255 / 255 + ')';
-		ctx.fillRect(x, y, 3, 3);
+	type WSEventHandler = (data: { data: string }) => void;
+	let id: string;
+	$: if (id) {
+		ws.removeEventListener('message', getCurrentGameData);
+		ws.addEventListener('message', playGame);
+	}
+	type Moves = { id?: string; color?: string; moves?: [{ x: number; y: number }] };
+	const getCurrentGameData: WSEventHandler = ({ data }) => {
+		const metadata: Moves = JSON.parse(data);
+		console.log(metadata);
+		if ('moves' in metadata) {
+			for (const move of metadata.moves || []) {
+				fillPixel(move?.x, move?.y, Number(metadata.color));
+			}
+		} else if ('id' in metadata) {
+			id = metadata.id as string;
+			Cookies.set('uid', id);
+		}
+	};
+	const playGame: WSEventHandler = ({ data }) => {
+		const info = JSON.parse(data);
+		fillPixel(info.mousePos.x, info.mousePos.y, info.color);
+	};
+
+	const fillPixel = (x: number, y: number, hue: number) => {
+		if (ctx) ctx.fillStyle = `hsl(${hue}, 100%, 50%)`;
+		ctx?.fillRect(x, y, 4, 4);
 	};
 	let mousePos: MousePos = { x: 0, y: 0 };
-	const handleMouseMove = (e) => {
+	const handleMouseMove = (e: MouseEvent) => {
 		mousePos.x = e.x;
 		mousePos.y = e.y;
-		ws.send(JSON.stringify({ player: 1, mousePos: mousePos }));
+		ws.send(JSON.stringify({ player: id, mousePos: mousePos }));
 	};
 	onMount(() => {
+		const uid = Cookies.get('uid') || '';
 		ctx = canvas.getContext('2d');
 		ws = new WebSocket('ws://localhost:8080');
-		ws.addEventListener('message', (data) => {
-			const info = JSON.parse(data.data);
-			console.log(info.mousePos);
-			fillPixel(info.mousePos.x, info.mousePos.y);
-		});
+		ws.addEventListener('message', getCurrentGameData);
+		ws.onopen = () => {
+			ws.send(JSON.stringify({ init: uid }));
+		};
 	});
 </script>
 
 <svelte:window on:mousemove={handleMouseMove} />
-<canvas bind:this={canvas} width={1000} height={1000} />
+<canvas bind:this={canvas} width={1200} height={800} />
 {mousePos.x.toString()}
 {mousePos.y.toString()}
 
@@ -38,6 +63,7 @@
 	canvas {
 		position: absolute;
 		z-index: -1;
+		overflow: hidden;
 		inset: 0;
 	}
 </style>
--- websockets/index.ts
+++ websockets/index.ts
@@ -1,28 +1,45 @@
 import { WebSocketServer } from 'ws';
+import { v4 as uuidv4 } from 'uuid';
 
 const wss = new WebSocketServer({ port: 8080 });
-const game = {
+interface move {
+	x: number;
+	y: number;
+}
+const clients = new Map();
+const game: { players: { [key: string]: { color: number; moves: move[] } } } = {
 	players: {
-		1: [
-			{
-				x: 0,
-				y: 0
-			}
-		],
-		2: [
-			{
-				x: 0,
-				y: 0
-			}
-		]
+		[uuidv4()]: { color: 0, moves: [{ x: 0, y: 0 }] }
 	}
 };
-
 wss.on('connection', function connection(ws) {
+	const id: string = uuidv4();
+	const color: number = Math.floor(Math.random() * 360);
+	const metadata = { id, color };
+	clients.set(ws, metadata);
+	game.players[id] = { color: 0, moves: [] };
+
 	ws.on('message', function message(data: string) {
-		const move = JSON.parse(data);
-		// game.players[move.player].push(move.mousePos);
-		broadcast(JSON.stringify(move));
+		const { color, id } = clients.get(ws);
+		const json = JSON.parse(data);
+		console.log(json);
+		if ('init' in json) {
+			if (json.init) {
+				clients.set(ws, { id: json.init, color: clients.get(ws).color });
+				if (!game.players[json.init]) {
+					game.players[json.init] = { moves: [], ...clients.get(ws) };
+				}
+			}
+			for (const player of Object.values(game.players)) {
+				ws.send(JSON.stringify({ color: player.color, moves: player.moves }));
+			}
+			ws.send(JSON.stringify({ color, id }));
+		} else {
+			const move: { mousePos: { x: number; y: number } } = JSON.parse(data);
+			game.players[id].moves.push(move.mousePos);
+			game.players[id].color = color;
+			broadcast(JSON.stringify({ ...move, color }));
+		}
 	});
 });
 function broadcast(msg: string) {
