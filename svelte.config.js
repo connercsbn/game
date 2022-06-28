2bd8616eec0b568f48e2613c42540445a1ea6f82
Conner <65339198+connercsbn@users.noreply.github.com>
2022-06-27 23:38:17 -0700

    use adapter-auto for vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node'
+import adapter from '@sveltejs/adapter-auto'
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,9 +8,7 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-      adapter: adapter({
-        out: 'public'
-      })
+      adapter: adapter()
     }
 }
 
